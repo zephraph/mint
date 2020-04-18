cdda333cf0261a32ca11fee6273a430202bc14b5
Gusztáv Szikszai <guszti5@hotmail.com>
2020-04-18 14:23:49 +0200

    Show hover for enum if it's an enum when hovering a type.

--- src/ls/hover/type.cr
+++ src/ls/hover/type.cr
@@ -2,10 +2,27 @@ module Mint
   module LS
     class Hover < LSP::RequestMessage
       def hover(node : Ast::Type, workspace) : Array(String | Nil)
-        type =
-          workspace.formatter.format(node)
+        enum_node =
+          workspace
+            .ast
+            .enums
+            .find(&.name.==(node.name))
 
-        [type] of String | Nil
+        if enum_node
+          hover(enum_node, workspace)
+        else
+          record =
+            workspace
+              .type_checker
+              .records
+              .find(&.name.==(node.name))
+              .try(&.to_pretty)
+
+          type =
+            workspace.formatter.format(node)
+
+          ["```\n#{record || type}\n```"] of String | Nil
+        end
       end
     end
   end
